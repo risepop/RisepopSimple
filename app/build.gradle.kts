f8f337bba93f423404b78bf4372dfff8f41c7cfe
pxx <985398647@qq.com>
2025-05-27 21:07:11 +0800

    开启混淆及签名

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,16 +16,29 @@ android {
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
-
-    buildTypes {
-        release {
-            isMinifyEnabled = false
-            proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
-            )
+    signingConfigs {
+        create("release") {
+            storeFile = file("../demo.jks")
+            storePassword = "123456"
+            keyAlias = "key0"
+            keyPassword = "123456"
         }
     }
+    buildTypes {
+        release {
+            isMinifyEnabled = true
+            signingConfig = signingConfigs.getByName("release")
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro"
+            )
+        }
+        debug {
+            isMinifyEnabled = true
+            signingConfig = signingConfigs.getByName("release")
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+        }
+    }
+
+
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_1_8
         targetCompatibility = JavaVersion.VERSION_1_8
@@ -36,7 +49,7 @@ android {
 }
 
 dependencies {
-
+    implementation(fileTree(mapOf("dir" to "libs", "include" to listOf("*.aar"))))
     implementation("androidx.core:core-ktx:1.13.1")
     implementation("androidx.appcompat:appcompat:1.7.0")
     implementation("com.google.android.material:material:1.12.0")
@@ -44,5 +57,18 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.2.1")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
-    implementation("com.github.risepop:resome:1.0.2")
+
+    implementation("com.github.risepop:resome:1.0.4")
+
+    //解析
+    implementation("com.google.code.gson:gson:2.8.6")
+//    网络请求
+    api("com.squareup.okhttp3:okhttp:3.12.13")
+    api("com.squareup.okhttp3:logging-interceptor:3.12.13")
+//    缓存框架
+    api("com.tencent:mmkv:1.3.7")
+
+    api("androidx.legacy:legacy-support-v4:1.0.0")
+    api("androidx.appcompat:appcompat:1.3.0-alpha02")
+
 }
