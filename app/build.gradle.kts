82caddc5238d160d63f588b3a4300f8dc3c4ba78
pxx <985398647@qq.com>
2025-05-28 23:46:43 +0800

    update simple

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -58,17 +58,7 @@ dependencies {
     androidTestImplementation("androidx.test.ext:junit:1.2.1")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
 
-    implementation("com.github.risepop:risepop-adx-android:1.0.6")
+    implementation("com.github.risepop:risepop-adx-android:1.0.7")
 
-    //解析
-    implementation("com.google.code.gson:gson:2.8.6")
-//    网络请求
-    api("com.squareup.okhttp3:okhttp:3.12.13")
-    api("com.squareup.okhttp3:logging-interceptor:3.12.13")
-//    缓存框架
-    api("com.tencent:mmkv:1.3.7")
-
-    api("androidx.legacy:legacy-support-v4:1.0.0")
-    api("androidx.appcompat:appcompat:1.3.0-alpha02")
 
 }
