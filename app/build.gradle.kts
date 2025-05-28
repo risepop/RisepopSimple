243a79c20cb7dff59cdfa9debacc99af3653aec3
pxx <985398647@qq.com>
2025-05-28 16:42:38 +0800

    update simple

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -58,7 +58,7 @@ dependencies {
     androidTestImplementation("androidx.test.ext:junit:1.2.1")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
 
-    implementation("com.github.risepop:resome:1.0.5")
+    implementation("com.github.risepop:risepop-adx-android:1.0.6")
 
     //解析
     implementation("com.google.code.gson:gson:2.8.6")
